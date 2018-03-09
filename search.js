bbebffa7dff4bf20fbf0daa93e8c94d8b5328f19
okputadora <okputadora@gmail.com>
2018-03-08 21:17:12 -0500

    grab input from text fields

--- search.js
+++ search.js
@@ -2,24 +2,27 @@
 $(document).ready(function(){
 
   $("#searchBtn").on("click", function(){
-    console.log("searching")
+    var searchTerm =  $("#searchTerm").val()
+    var numRecords = $("#numRecords").val()
+    var startYear = $("#startYear").val()
+    var endYear = $("#endYear").val()
+    console.log(endYear)
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "4e8ab05444a446ce9597076968189154",
+      'q': searchTerm,
+      'begin_date': startYear + "-01-01",
+      'end_date': endYear + "-01-01",
+      'sort': "newest",
+      'page': 0
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
   })
-  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-  var query =
-  url += '?' + $.param({
-    'api-key': "4e8ab05444a446ce9597076968189154",
-    'q': "elon%20musk",
-    'begin_date': "2000-01-01",
-    'end_date': "2010-01-01",
-    'sort': "newest",
-    'page': 0
-  });
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
-    console.log(result);
-  }).fail(function(err) {
-    throw err;
-  });
 })
