beac9dd6dcb8d9ea0ad9d1f366a5cb14371c950c
okputadora <okputadora@gmail.com>
2018-03-08 20:37:11 -0500

    basic ajax request

---
+++ search.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "4e8ab05444a446ce9597076968189154",
+    'q': "elon%20musk",
+    'begin_date': "2000-01-01",
+    'end_date': "2010-01-01",
+    'sort': "newest",
+    'page': 0
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function(err) {
+    throw err;
+  });
+})
